c74f94ce86f46b30967bf7048408a6ae42bf463c
Tony Allevato <allevato@google.com>
2013-12-17 15:18:02 -0800

    Added voice trigger ("show me a demo"); updated code style to match that of other samples.

--- README.md
+++ README.md
@@ -1,48 +1,22 @@
-apk-waveform-sample
-=====================
+Waveform
+========
 
-A simple Glassware apk that receives audio input from the microphone and
-displays it on the screen as an animated waveform, along with the decibel
+This sample launches an immersion that receives audio input from the microphone
+and displays it on the screen as an animated waveform, along with the decibel
 level.
 
-## Prerequistes
+## Getting started
 
-* [Android SDK](http://developer.android.com/sdk/index.html) - This is needed for development.
-* [Glass](http://www.google.com/glass/start/how-to-get-one/) - You can push the code to an Android device but the goal of this project is to get started writing .apk for Glass.
+Check out our documentation to learn how to get started on
+https://developers.google.com/glass/develop/index
 
-## Importing and compiling the code
+## Running the sample on Glass
 
-Checkout the code into your filesystem and use your favorite IDE to import and compile the code.
+You can use your IDE to compile and install the sample or use
+[`adb`](https://developer.android.com/tools/help/adb.html)
+on the command line:
 
-### In Eclipse and ADT
+    $ adb install -r WaveformSample.apk
 
-1. Click **File > Import ... > Existing Android Code into Workspace**.
-2. Click **Browse**, select the location of the `apk-waveform-sample/` directory, and click **OK**.
-3. Select the **apk-waveform-sample** project to import it and select
-**Copy projects into workspace**.
-6. Click **Finish** to complete the import.
-
-### In Android Studio
-
-1. Checkout the sample to a location where you keep your Android
-development projects. A `apk-waveform-sample/` directory is created.
-2. Click **File > Import Project...**, select the location of the `apk-waveform-sample/` directory, and click **Next** until you see the **Finish** button.
-3. Click **Finish** to complete the import.
-
-
-## Turning on debug mode
-
-You'll need to turn on debug mode before you do any type of development on
-Glass. Go to **Settings > Device Info > Turn on debug** to enable it.
-
-## Push the .apk on Glass
-
-You can use your IDE to compile, push and run the .apk or use this command line with `adb`:
-
-    adb install -r apk-waveform-sample.apk
-
-## Run the .apk
-
-You can use your IDE to run the .apk or use this command line with `adb` when the Glass screen is **on**:
-
-    adb shell am start -n com.google.glass.samples.waveform/.WaveformActivity
+To start the sample, say "ok glass, show me a demo" from the Glass clock
+screen or use the touch menu.
---
+++ src/com/google/android/glass/sample/waveform/WaveformView.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.android.glass.sample.waveform;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+import java.util.LinkedList;
+
+/**
+ * A view that displays audio data on the screen as a waveform.
+ */
+public class WaveformView extends SurfaceView {
+
+    // The number of buffer frames to keep around (for a nice fade-out visualization).
+    private static final int HISTORY_SIZE = 6;
+
+    // To make quieter sounds still show up well on the display, we use +/- 8192 as the amplitude
+    // that reaches the top/bottom of the view instead of +/- 32767. Any samples that have
+    // magnitude higher than this limit will simply be clipped during drawing.
+    private static final float MAX_AMPLITUDE_TO_DRAW = 8192.0f;
+
+    // The queue that will hold historical audio data.
+    private final LinkedList<short[]> mAudioData;
+
+    private final Paint mPaint;
+
+    public WaveformView(Context context) {
+        this(context, null, 0);
+    }
+
+    public WaveformView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public WaveformView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        mAudioData = new LinkedList<short[]>();
+
+        mPaint = new Paint();
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.WHITE);
+        mPaint.setStrokeWidth(0);
+        mPaint.setAntiAlias(true);
+    }
+
+    /**
+     * Updates the waveform view with a new "frame" of samples and renders it. The new frame gets
+     * added to the front of the rendering queue, pushing the previous frames back, causing them to
+     * be faded out visually.
+     *
+     * @param buffer the most recent buffer of audio samples
+     */
+    public synchronized void updateAudioData(short[] buffer) {
+        short[] newBuffer;
+
+        // We want to keep a small amount of history in the view to provide a nice fading effect.
+        // We use a linked list that we treat as a queue for this.
+        if (mAudioData.size() == HISTORY_SIZE) {
+            newBuffer = mAudioData.removeFirst();
+            System.arraycopy(buffer, 0, newBuffer, 0, buffer.length);
+        } else {
+            newBuffer = buffer.clone();
+        }
+
+        mAudioData.addLast(newBuffer);
+
+        // Update the display.
+        Canvas canvas = getHolder().lockCanvas();
+        if (canvas != null) {
+            drawWaveform(canvas);
+            getHolder().unlockCanvasAndPost(canvas);
+        }
+    }
+
+    /**
+     * Repaints the view's surface.
+     *
+     * @param canvas the {@link Canvas} object on which to draw
+     */
+    private void drawWaveform(Canvas canvas) {
+        // Clear the screen each time because SurfaceView won't do this for us.
+        canvas.drawColor(Color.BLACK);
+
+        float width = getWidth();
+        float height = getHeight();
+        float centerY = height / 2;
+
+        // We draw the history from oldest to newest so that the older audio data is further back
+        // and darker than the most recent data.
+        int colorDelta = 255 / (HISTORY_SIZE + 1);
+        int brightness = colorDelta;
+
+        for (short[] buffer : mAudioData) {
+            mPaint.setColor(Color.argb(brightness, 128, 255, 192));
+
+            float lastX = -1;
+            float lastY = -1;
+
+            // For efficiency, we don't draw all of the samples in the buffer, but only the ones
+            // that align with pixel boundaries.
+            for (int x = 0; x < width; x++) {
+                int index = (int) ((x / width) * buffer.length);
+                short sample = buffer[index];
+                float y = (sample / MAX_AMPLITUDE_TO_DRAW) * centerY + centerY;
+
+                if (lastX != -1) {
+                    canvas.drawLine(lastX, lastY, x, y, mPaint);
+                }
+
+                lastX = x;
+                lastY = y;
+            }
+
+            brightness += colorDelta;
+        }
+    }
+}
